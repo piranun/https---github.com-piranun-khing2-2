cf5373c9b3c42288879bf28ed95d3d251276d6c0
khing66-2 <khingkhidux@gmail.com>
2023-11-07 10:36:08 +0700

    variable.php

--- Variable.php
+++ Variable.php
@@ -12,4 +12,10 @@
     $sum = $num1 + $num2 + $num3 + $num4;
     echo "sum = ". $sum ."<br>";
     $avg = $sum / 4;
-    echo  "avg =". $avg ;
+    echo "x". $avg ."<br>";
+
+    #x++ คือการที่ x+1 จากนั้นเก็บค่าใน x เหมือนเดิม
+    $x = 5;
+    echo "x=". $x ."<br>";
+    $x++ ;
+    echo "x=". $x ."<br>";
